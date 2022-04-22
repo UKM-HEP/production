17759bcd7e6f6219495aed9545ca13fd4ae6cbbc
SiewYan <siew.yan.hoh@cern.ch>
2022-04-22 12:26:01 +0800

    first commit

---
+++ catalogue.py
@@ -0,0 +1,173 @@
+datasets = {
+    # Higgs-to-four-lepton analysis
+    # http://opendata.cern.ch/record/5500
+    'DoubleElectron/Run2011A-12Oct2013-v1/AOD' :
+    {
+        'data' : True,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.MQXP.QNMB',
+    },
+    
+    'DoubleMu/Run2011A-12Oct2013-v1/AOD' :
+    {
+        'data' : True,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.RZ34.QR6N',
+    },
+    
+    'DoubleMuParked/Run2012B-22Jan2013-v1/AOD' :
+    {
+        'data' : True,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.YLIC.86ZZ',
+    },
+
+    'DoubleMuParked/Run2012C-22Jan2013-v1/AOD' :
+    {
+        'data' : True,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.M5AD.Y3V3',
+    },
+
+    'DoubleElectron/Run2012B-22Jan2013-v1/AOD' :
+    {
+        'data' : True,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.S0H8.LBD3',
+    },
+
+    'DoubleElectron/Run2012C-22Jan2013-v1/AOD' :
+    {
+        'data' : True,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.SINM.BV86',
+    },
+    
+    'ZZTo4mu_mll4_7TeV-powheg-pythia6' :
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.XWVK.M4VG',
+    },
+    
+    'ZZTo4e_mll4_7TeV-powheg-pythia6' :
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.95DX.4BMP',
+    },
+    
+    'ZZTo2e2mu_mll4_7TeV-powheg-pythia6' :
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.7G2E.4PGB',
+    },
+    
+    'SMHiggsToZZTo4L_M-125_7TeV-powheg15-JHUgenV3-pythia6' :
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.K9EW.KRDS',
+    },
+    
+    'DYJetsToLL_M-50_7TeV-madgraph-pythia6-tauola' :
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.4475.SSXE',
+    },
+    
+    'DYJetsToLL_M-10To50_TuneZ2_7TeV-pythia6' :
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.T8RZ.D52D',
+    },
+    
+    'TTTo2L2Nu2B_7TeV-powheg-pythia6' :
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.CSJG.AWBA',
+    },
+    
+    'ZZTo4mu_8TeV-powheg-pythia6' :
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.HJ1F.5U9R',
+    },
+
+    'ZZTo4e_8TeV-powheg-pythia6' :
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.PEOD.ZZJY',
+    },
+
+    'ZZTo2e2mu_8TeV-powheg-pythia6' :
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.461G.HELP',
+    },
+
+    'SMHiggsToZZTo4L_M-125_8TeV-powheg15-JHUgenV3-pythia6':
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.G13X.TDSB',
+    },
+
+    'DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball-tauola-tauPolarOff':
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.DRSP.TO3O',
+    },
+
+    'DYJetsToLL_M-10to50_HT-200to400_TuneZ2star_8TeV-madgraph-tauola' :
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.F7AB.F8SV',
+    },
+
+    'DYJetsToLL_M-10to50_HT-400toInf_TuneZ2star_8TeV-madgraph-tauola' :
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.H6M1.8471',
+    },
+
+    'TTbar_8TeV-Madspin_aMCatNLO-herwig' :
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.XH95.JNSE',
+    },
+    #########
+    
+    'W3JetsToLNu_TuneZ2Star_8TeV-madgraph' :
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.HHCJ.TVXH',
+    },
+
+    'W1JetsToLNu_TuneZ2Star_8TeV-madgraph' :
+    {
+        'data' : False,
+	'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.REHM.JKUH',
+    },
+
+    'W2JetsToLNu_TuneZ2Star_8TeV-madgraph' :
+    {
+        'data' : False,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.DELK.2V7R',
+    },
+
+        'MuOnia/Run2012B-22Jan2013-v1/AOD' :
+    {
+        'data' : True,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.Q4T1.HLNS',
+    },
+    
+    'MuOnia/Run2012C-22Jan2013-v1/AOD' :
+    {
+        'data' : True,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.Q4T1.HLNS',
+    },
+
+    'MuOniaParked/Run2012B-22Jan2013-v1/AOD' :
+    {
+        'data' : True,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.ZCFQ.Q557',
+    },
+
+    'MuOniaParked/Run2012C-22Jan2013-v1/AOD' :
+    {
+        'data' : True,
+        'doi' : 'http://doi.org/10.7483/OPENDATA.CMS.89LJ.6VKB',
+    },
+    
+}
